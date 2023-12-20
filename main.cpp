8a610eb40f3b201d69e49d22e44484965f6dfc51
Matth-M <matthieu.matsima@gmail.com>
2023-12-20 23:29:41 +0100

    Add white background

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,6 +21,21 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  // Create renderer
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED || SDL_RENDERER_SOFTWARE);
+  if (renderer == NULL) {
+    printf("Unable to create renderer. Error %s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Set color for next operation
+  SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+  // Clear whole rendering target with previously selected color
+  SDL_RenderClear(renderer);
+  // Update screen
+  SDL_RenderPresent(renderer);
+
   SDL_Event e;
   bool running = true;
   while (running) {
@@ -34,6 +50,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  SDL_DestroyRenderer(renderer);
   // Destroy the window and shutdown the program
   SDL_DestroyWindow(window);
   SDL_Quit();
