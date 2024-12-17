98130bbab1c3bd7aa0c22af8f854921bdb3d01e3
Matth-M <matthieu.matsima@gmail.com>
2024-12-17 02:47:37 +0100

    Use member initialization list

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,11 +1,6 @@
 #include "GameState.h"
 
 GameState::GameState(bool state, bool spawn, Player &player, bool jump)
-    : player(player) {
-  this->running = state;
-  this->spawnEnvironment = spawn;
-  this->player = player;
-  this->jump = jump;
-}
+    : player(player), running(state), spawnEnvironment(spawn), jump(jump) {}
 
 GameState::~GameState() {}
