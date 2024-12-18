71a1fd1787fe15164098ea8e13282b4089d2eb77
Matth-M <matthieu.matsima@gmail.com>
2024-12-18 03:08:16 +0100

    Reorder initialization

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,6 +1,6 @@
 #include "GameState.h"
 
 GameState::GameState(bool running, bool spawnEnvironment, Player &player, bool jump)
-    : player(player), running(running), spawnEnvironment(spawnEnvironment), jump(jump) {}
+    : running(running), spawnEnvironment(spawnEnvironment), jump(jump), player(player) {}
 
 GameState::~GameState() {}
