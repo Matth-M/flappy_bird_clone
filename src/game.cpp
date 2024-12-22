a9ccb20c7d3ceb54c4babf306855c0f17756e5b7
Matth-M <matthieu.matsima@gmail.com>
2024-12-22 04:54:00 +0100

    Spawn obstacles

--- src/game.cpp
+++ src/game.cpp
@@ -1,9 +1,12 @@
 #include "game.h"
+#include "Entity.h"
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
+#include <algorithm>
 #include <chrono>
 #include <thread>
+#include <vector>
 
 // Handle keyboard events
 void handleEvents(GameState &state) {
@@ -31,6 +34,9 @@ void drawRectangle(SDL_Renderer *renderer, const SDL_Rect &rect, Uint8 r,
 
 void gameLoop(GameState state, SDL_Renderer *renderer) {
 
+  uint64_t cycle_iteration = 0;
+  std::vector<Entity> obstacles;
+
   while (state.running) {
 
     // Pause the thread for a bit to make the game playable by humans
@@ -43,19 +49,39 @@ void gameLoop(GameState state, SDL_Renderer *renderer) {
 
     handleEvents(state);
 
+    // Remove obstacles past the player
+    obstacles.erase(std::remove_if(obstacles.begin(), obstacles.end(),
+                                   [](const Entity &obstacle) {
+                                     return obstacle.hitbox.x <= 0;
+                                   }),
+                    obstacles.end());
+
+    // Draw obstacles
+    for (auto it = begin(obstacles); it != end(obstacles); ++it) {
+      it->updatePos();
+      drawRectangle(renderer, it->hitbox, 0xff, 0, 0);
+    }
+
+    if (cycle_iteration % 50 == 0) {
+      const auto w = 5;
+      const auto h = WINDOW_HEIGHT / 4;
+      SDL_Rect obstacle_hitbox =
+          SDL_Rect{.x = WINDOW_WIDTH - w, .y = 0, .w = w, .h = h};
+      // Obstacles are moving towards the left
+      auto obstacle_speed = -10;
+      Entity obstacle = Entity(obstacle_hitbox, obstacle_speed, 0);
+      obstacles.push_back(obstacle);
+    }
+
     // Draw player
-    drawRectangle(renderer, state.player.body.hitbox, 0xff, 0, 0);
+    drawRectangle(renderer, state.player.body.hitbox, 0, 0xff, 0);
     state.player.body.updatePos();
     state.player.body.updateAccel(0, GRAVITY);
 
-    if (state.spawnEnvironment) {
-      SDL_Rect env = SDL_Rect{
-          .x = WINDOW_WIDTH / 2, .y = WINDOW_HEIGHT / 2, .w = 16, .h = 27};
-      drawRectangle(renderer, env, 0xff, 0, 0x00);
-    }
     if (state.jump) {
       state.player.jump();
       state.jump = !state.jump;
     }
+    cycle_iteration++;
   }
 }
